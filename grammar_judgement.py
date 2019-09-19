1a88c6071404b2eefc4eb909cf3c59f4a87bcf60
sumimoto satosi <ht17a048@oecu.jp>
2019-09-19 09:28:35 +0900

    第一文型の追加

--- grammar_judgement.py
+++ grammar_judgement.py
@@ -1,4 +1,17 @@
 import nltk
+import re
+
 iinput_text =input("文字を入力してください。")
+
 text =nltk.word_tokenize(str(iinput_text))
-print(nltk.pos_tag(text))
+#第一文型の判定の文
+tag_text =nltk.pos_tag(text)
+#主語の判定名詞と代名詞
+if re.search('(NN|NNP|NNS|PRP)',tag_text[0][1]):
+    print('S:主語')
+print(tag_text[0][1])
+#There isの文法項目を書いてみる
+# if re.search('^There ' '+is|are',text) :
+#     print('sucess')
+print('品詞',nltk.pos_tag(text))
+print('文型',)
