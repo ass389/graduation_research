ec4b3648d2491f34b81671233003101db64457a1
sumimoto satosi <ht17a048@oecu.jp>
2019-09-27 21:55:55 +0900

    第一文型を判定する処理を追加

--- grammar_judgement.py
+++ grammar_judgement.py
@@ -4,8 +4,9 @@ import re
 iinput_text =input("文字を入力してください。")
 
 text =nltk.word_tokenize(str(iinput_text))
-
 tag_text =nltk.pos_tag(text)
+sentence_p =[]
+sentence=''
 """
     第一文型の判定
     1語の場合
@@ -25,24 +26,29 @@ tag_text =nltk.pos_tag(text)
 """
 if re.search('(NN|NNP|NNS|PRP|DT)',tag_text[0][1]):
     print('S:主語')
+    sentence_p.append('S')
     if re.search('VB|VBN',tag_text[1][1]):
-        print('V:動詞')
+        sentence_p.append('V')
+        print(sentence_p)
 #2語の場合
 elif re.search('(PRP$|DT)',tag_text[0][1])and re.search('(NN|JJ)'):
-    print('S:主語')
+    sentence_p.append('S')
     if re.search('VB|VB'):
-        print('V:動詞')
+        sentence_p.append('V')
 elif re.search('To',text[0][1])and re.search('VB|VBN'):
-    print('S:主語')
+    sentence_p.append('S')
     if re.search('VB|VBN'):
-        print('V:動詞')
+        sentence_p.append('V')
 elif re.search('VG|VN',tag_text[0][1])+re.search('',tag_text[1][1]):
-    print('S:主語')
+    sentence_p.append('S')
     if re.search('VB|VBN'):
-        print('V:動詞')
+        sentence_p.append('V')
 
 #主語が3語以上の場合
 
 
+#第一文型の出力
+if re.search('S',sentence_p[0]) and re.search('V',sentence_p[1]):
+    sentence ='第一文型'
 print('',nltk.pos_tag(text))
-print('文型',)
+print('文型',sentence)
