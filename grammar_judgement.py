6c279f24c01747e7a839a015a4975364a35a524d
sumimoto satosi <ht17a048@oecu.jp>
2019-09-30 23:11:31 +0900

    文法判定のところを関数化、ファイルを読み込んで、一文ずつ判定できるようにした。

--- grammar_judgement.py
+++ grammar_judgement.py
@@ -1,12 +1,6 @@
 import nltk
 import re
 
-iinput_text =input("文字を入力してください。")
-
-text =nltk.word_tokenize(str(iinput_text))
-tag_text =nltk.pos_tag(text)
-sentence_p =[]
-sentence=''
 """
     第一文型の判定
     1語の場合
@@ -24,31 +18,46 @@ sentence=''
     動詞の判定
 
 """
-if re.search('(NN|NNP|NNS|PRP|DT)',tag_text[0][1]):
-    print('S:主語')
-    sentence_p.append('S')
-    if re.search('VB|VBN',tag_text[1][1]):
-        sentence_p.append('V')
-        print(sentence_p)
-#2語の場合
-elif re.search('(PRP$|DT)',tag_text[0][1])and re.search('(NN|JJ)'):
-    sentence_p.append('S')
-    if re.search('VB|VB'):
-        sentence_p.append('V')
-elif re.search('To',text[0][1])and re.search('VB|VBN'):
-    sentence_p.append('S')
-    if re.search('VB|VBN'):
-        sentence_p.append('V')
-elif re.search('VG|VN',tag_text[0][1])+re.search('',tag_text[1][1]):
-    sentence_p.append('S')
-    if re.search('VB|VBN'):
-        sentence_p.append('V')
+def judgemnt(text):
+    sentence_p=[]
+    sentence= ''
+    if re.search('(NN|NNP|NNS|PRP|DT)',text[0][1]):
+        sentence_p.append('S')
+        if re.search('VB|VBN',text[1][1]):
+            sentence_p.append('V')
+            print(sentence_p)
+    #2語の場合
+    elif re.search('(PRP$|DT)',text[0][1])and re.search('(NN|JJ)',text[1][1]):
+        sentence_p.append('S')
+        if re.search('VB|VB',text[2][1]):
+            sentence_p.append('V')
+    elif re.search('To',text[0][1])and re.search('VB|VBN',text[1][1]):
+        sentence_p.append('S')
+        if re.search('VB|VBN',text[2][1]):
+            sentence_p.append('V')
+    elif re.search('(VG|VN)',text[0][1])and re.search('(NN|NNP)',text[1][1]):
+        sentence_p.append('S')
+        if re.search('VB|VBN',text[2][1]):
+            sentence_p.append('V')
 
-#主語が3語以上の場合
+    #主語が3語以上の場合
+    #第一文型の出力
+    if len(sentence_p)>=2:
+        if re.search('S',sentence_p[0]) and re.search('V',sentence_p[1]):
+            sentence ='第一文型'
+            return sentence
+        else:
+            return 0
 
 
-#第一文型の出力
-if re.search('S',sentence_p[0]) and re.search('V',sentence_p[1]):
-    sentence ='第一文型'
-print('',nltk.pos_tag(text))
-print('文型',sentence)
+f =open("test.txt",'r')
+for line in f:
+    input_text =line
+    text =nltk.word_tokenize(str(input_text))
+    print(text)
+    tag_text =nltk.pos_tag(text)
+    sentence =''
+    print(tag_text)
+    sentence =judgemnt(tag_text)
+    print('トークン化した文章:',nltk.pos_tag(text))
+    print('文型:',sentence)
