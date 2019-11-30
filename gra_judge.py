891e7458ebf68eb0266449a352d7f54929f66935
sumimoto satosi <ht17a048@oecu.jp>
2019-11-30 22:40:57 +0900

    テキストデータの名前の変更

--- gra_judge.py
+++ gra_judge.py
@@ -23,12 +23,45 @@ from nltk import Tree
 def judgemnt(text):
     sentence_p=[]
     sentence= ''
-    if re.search('(NN.?|PRP|DT|EX)',text[0][1]):
+    #第二語の場合
+    if re.search('DT|TO|PRP.?',text[0][1]) and re.search('JJ|NN.?',text[1][1]):
+        sentence_p.append('S')
+        print('sにしました')
+        if re.search('VB.?',text[2][1]):
+            sentence_p.append('V')
+            if re.search('JJ.?',text[3][1]):
+                sentence_p.append('C')
+            elif re.search('RB|DT',text[3][1]) and re.search('JJ|NN',text[4][1]):
+                sentence_p.append('C')
+        #第三文型
+            elif re.search('NN.?|PRP.?|VBG',text[3][1]):
+                sentence_p.append('O')
+                #第四文型
+                if re.search('NN.?',text[4][1]):
+                    sentence_p.append('O')
+            #第五文型
+                elif re.search('JJ.?|VB.?',text[4][1]):
+                    sentence_p.append('C')
+        elif re.search('MD|VB.?',text[2][1]) and re.search('VB.?',text[3][1]):
+            sentence_p.append('V')
+            if re.search('JJ.?',text[4][1]):
+                sentence_p.append('C')
+        #第三文型
+            elif re.search('NN.?|PRP.?|VBG',text[4][1]):
+                sentence_p.append('O')
+                #第四文型
+                if re.search('NN.?',text[5][1]):
+                    sentence_p.append('O')
+            #第五文型
+                elif re.search('JJ.?',text[5][1]):
+                    sentence_p.append('C')
+
+    elif re.search('(NN.?|PRP|DT|EX)',text[0][1]):
         sentence_p.append('S')
         #第二文型
         if re.search('VB.?',text[1][1]):
             sentence_p.append('V')
-            if re.search('JJ.?',text[3][1]):
+            if re.search('JJ.?',text[2][1]):
                 sentence_p.append('C')
         #第三文型
             elif re.search('NN.?|PRP.?|VBG',text[2][1]):
@@ -52,36 +85,6 @@ def judgemnt(text):
                     sentence_p.append('O')
             #第五文型
                 elif re.search('JJ.?',text[4][1]):
-                    sentence_p.append('C')
-
-    #第２文の場合
-    elif re.search('DT|TO|PRP.?',text[0][1]) and re.search('JJ|NN.?|VB.?',text[1][1]):
-        sentence_p.append('S')
-        if re.search('VB.?',text[2][1]):
-            sentence_p.append('V')
-            if re.search('JJ.?',text[3][1]):
-                sentence_p.append('C')
-        #第三文型
-            elif re.search('NN.?|PRP.?|VBG',text[3][1]):
-                sentence_p.append('O')
-                #第四文型
-                if re.search('NN.?',text[4][1]):
-                    sentence_p.append('O')
-            #第五文型
-                elif re.search('JJ.?',text[4][1]):
-                    sentence_p.append('C')
-        elif re.search('MD|VB.?',text[2][1]) and re.search('VB.?',text[3][1]):
-            sentence_p.append('V')
-            if re.search('JJ.?',text[4][1]):
-                sentence_p.append('C')
-        #第三文型
-            elif re.search('NN.?|PRP.?|VBG',text[4][1]):
-                sentence_p.append('O')
-                #第四文型
-                if re.search('NN.?',text[5][1]):
-                    sentence_p.append('O')
-            #第五文型
-                elif re.search('JJ.?',text[5][1]):
                     sentence_p.append('C')
 
     #第一文型の出力
@@ -130,6 +133,6 @@ for line in f:
     # result = cp.parse(tag_text)
     # t =nltk.Tree.fromstring(str(result))
     sentence =judgemnt(tag_text)
-    # print('トークン化した文章:',nltk.pos_tag(text))
-    # print('文型:',sentence)
-    print(sentence)
+    print('トークン化した文章:',nltk.pos_tag(text))
+    print('文型:',sentence)
+    # print(sentence)
