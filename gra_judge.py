476146056182d0c2d5b8081f161daf63448ff547
sumimoto satosi <ht17a048@oecu.jp>
2019-12-09 02:15:12 +0900

    現時点の最終版として確定

--- gra_judge.py
+++ gra_judge.py
@@ -24,7 +24,7 @@ def judgemnt(text):
     sentence_p=[]
     sentence= ''
     #第二語の場合
-    if re.search('DT|TO|PRP.?',text[0][1]) and re.search('JJ|NN.?|VB.?',text[1][1]):
+    if re.search('DT|TO|PRP.?|CD',text[0][1]) and re.search('JJ|NN.?',text[1][1]):
         sentence_p.append('S')
 
         if re.search('VB.?',text[2][1]):
@@ -47,6 +47,15 @@ def judgemnt(text):
             if re.search('JJ.?',text[4][1]):
                 sentence_p.append('C')
         #第三文型
+            elif re.search('NN.?|PRP.?|VBG',text[4][1]) and re.search('NN',text[5][1]):
+                sentence_p.append('O')
+                #第四文型
+                if re.search('NN?',text[6][1]):
+                    sentence_p.append('O')
+            #第五文型
+                elif re.search('JJ.|NNP',text[6][1]):
+                    sentence_p.append('C')
+        #Oの判定第二語
             elif re.search('NN.?|PRP.?|VBG',text[4][1]):
                 sentence_p.append('O')
                 #第四文型
@@ -55,7 +64,7 @@ def judgemnt(text):
             #第五文型
                 elif re.search('JJ.?',text[5][1]):
                     sentence_p.append('C')
-
+    #S第一語の場合
     elif re.search('(NN.?|PRP|DT|EX)',text[0][1]):
         sentence_p.append('S')
         #第二文型
@@ -63,14 +72,29 @@ def judgemnt(text):
             sentence_p.append('V')
             if re.search('JJ.?',text[2][1]):
                 sentence_p.append('C')
-        #第三文型
+        #Oの判定 第二語
+            elif re.search('NN.?|PRP.?|VBG',text[2][1])and re.search('NN',text[3][1]):
+                sentence_p.append('O')
+                #第四文型
+                if re.search('NN|DT?',text[4][1])and re.search('NN.?',text[5][1]):
+                    sentence_p.append('O')
+            #第五文型
+                elif re.search('JJ.|NN.?',text[4][1]):
+                    sentence_p.append('C')
+                #第四文型
+                if re.search('NN',text[4][1]):
+                    sentence_p.append('O')
+            #第五文型
+                elif re.search('JJ.|NN.?',text[4][1]):
+                    sentence_p.append('C')
+            #Oの判定
             elif re.search('NN.?|PRP.?|VBG',text[2][1]):
                 sentence_p.append('O')
                 #第四文型
-                if re.search('NN.?',text[3][1]):
+                if re.search('NNS',text[3][1]):
                     sentence_p.append('O')
             #第五文型
-                elif re.search('JJ.?',text[3][1]):
+                elif re.search('JJ.|NN.?',text[3][1]):
                     sentence_p.append('C')
         elif re.search('MD|VB.?',text[1][1]) and re.search('VB.?',text[2][1]):
             sentence_p.append('V')
@@ -85,6 +109,15 @@ def judgemnt(text):
                     sentence_p.append('O')
             #第五文型
                 elif re.search('JJ.?',text[4][1]):
+                    sentence_p.append('C')
+             #第三文型
+            elif re.search('NN.?|PRP.?|VBG',text[3][1]):
+                sentence_p.append('O')
+                #第四文型
+                if re.search('NN.?',text[4][1]):
+                    sentence_p.append('O')
+            #第五文型
+                elif re.search('JJ.?|VB.?',text[4][1]):
                     sentence_p.append('C')
 
     #第一文型の出力
